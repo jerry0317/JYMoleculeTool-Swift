084eb24055f9e54a01137fe2c1468d9e5028e57d
jerry0317 <yanjieyu1999@hotmail.com>
2019-06-20 12:39:51 -0700

    Initial Commit
    This is the Swift version of JYMoleculeTool (which is built on Python 2/3). The motivation of building a Swift version is to optimize performance and provides room for machine learning in the future. Still under development.

---
+++ JYMoleculeTool-Swift/mTools.swift
@@ -0,0 +1,9 @@
+//
+//  mTools.swift
+//  JYMoleculeTool-Swift
+//
+//  Created by Jerry Yan on 6/20/19.
+//  Copyright © 2019 Jerry Yan. All rights reserved.
+//
+
+import Foundation
---
+++ JYMoleculeTool-Swift/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  JYMoleculeTool-Swift
+//
+//  Created by Jerry Yan on 6/20/19.
+//  Copyright © 2019 Jerry Yan. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
