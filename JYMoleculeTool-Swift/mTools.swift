6fe9a55bbcd0949f0df54c04a432e440680eea41
Jerry Yan <jerry0317@users.noreply.github.com>
2019-06-21 00:20:13 -0700

    Add more basic methods migrated from Python

--- JYMoleculeTool-Swift/mTools.swift
+++ JYMoleculeTool-Swift/mTools.swift
@@ -7,11 +7,12 @@
 //
 
 import Foundation
+import Accelerate
 
 /**
  Position vector (three-dimensional)
  */
-class Vector{
+struct Vector3D{
     var x: Double
     var y: Double
     var z: Double
@@ -22,10 +23,26 @@ class Vector{
         self.z = z
     }
     
+    init(_ dictVec: [Double]){
+        self.init()
+        self.dictVec = dictVec
+    }
+    
+    var dictVec: [Double] {
+        get {
+            return [x, y, z]
+        }
+        set(newDictVec) {
+            x = newDictVec[0]
+            y = newDictVec[1]
+            z = newDictVec[2]
+        }
+    }
+    
 }
 
-extension Vector: Hashable {
-    static func == (lhs: Vector, rhs: Vector) -> Bool {
+extension Vector3D: Hashable {
+    static func == (lhs: Vector3D, rhs: Vector3D) -> Bool {
         return
             lhs.x == rhs.x &&
                 lhs.y == rhs.y &&
@@ -37,12 +54,66 @@ extension Vector: Hashable {
         hasher.combine(y)
         hasher.combine(z)
     }
+    
+}
+
+prefix operator -
+extension Vector3D {
+    /**
+     Inverse Vector
+     */
+    static prefix func - (vector: inout Vector3D) -> Vector3D {
+        return Vector3D(-vector.x, -vector.y, -vector.z)
+    }
+}
+
+infix operator +: AdditionPrecedence
+infix operator -: AdditionPrecedence
+infix operator .*: MultiplicationPrecedence
+extension Vector3D {
+    /**
+     Vector Addition
+     */
+    static func + (left: Vector3D, right: Vector3D) -> Vector3D {
+        return Vector3D(left.x + right.x, left.y + right.y, left.z + right.z)
+    }
+    /**
+     Vector Subtraction
+     */
+    static func - (left: Vector3D, right: Vector3D) -> Vector3D {
+        return Vector3D(left.x - right.x, left.y - right.y, left.z - right.z)
+    }
+    /**
+     Dot Product
+     */
+    static func .* (left: Vector3D, right: Vector3D) -> Double {
+        return left.x * right.x + left.y * right.y + left.z * right.z
+    }
+}
+
+extension Vector3D {
+    /**
+     Resign the vector based on |x|, |y|, and |z|.
+     */
+    func resign() -> [Vector3D] {
+        var possibleList: [Vector3D] = []
+        let signList = [[1,1,1], [1,1,-1], [1,-1,1], [1,-1,-1], [-1,1,1], [-1,1,-1], [-1,-1,1], [-1,-1,-1]]
+        for sign in signList {
+            var newDict: [Double] = [0,0,0]
+            for i in [0,1,2] {
+                newDict[i] = dictVec[i] * Double(sign[i])
+            }
+            let newRVec = Vector3D(newDict)
+            possibleList.append(newRVec)
+        }
+        return possibleList
+    }
 }
 
 /**
  Atom
  */
-class Atom {
+struct Atom {
     /**
      The name of the atom.
      */
@@ -51,9 +122,9 @@ class Atom {
     /**
      The position vector of the atom.
      */
-    var rvec: Vector?
+    var rvec: Vector3D?
     
-    init(_ name: String, _ rvec: Vector? = nil){
+    init(_ name: String, _ rvec: Vector3D? = nil){
         self.name = name
         self.rvec = rvec
     }
@@ -75,7 +146,7 @@ extension Atom: Hashable {
 /**
  Chemical bond type (between two atoms)
  */
-class ChemBondType {
+struct ChemBondType {
     /**
      The names of the two atoms in the chemical bond.
      */
@@ -154,7 +225,7 @@ extension ChemBondType: Hashable {
 /**
  Chemical bond between two atoms.
  */
-class ChemBond {
+struct ChemBond {
     /**
      The atoms engaged in the chemical bond.
      */
@@ -191,7 +262,7 @@ extension ChemBond: Hashable {
 /**
  Chemical bond graph constructed by multiple bonds
  */
-class ChemBondGraph {
+struct ChemBondGraph {
     /**
      The bonds engaged in this bond graph.
      */
@@ -228,7 +299,7 @@ extension ChemBondGraph: Hashable {
 /**
  Structural molecule: a not necessarily meaningful "molecule" with atoms constrained by serveral possible bond graphs
  */
-class StrcMolecule {
+struct StrcMolecule {
     /**
      The atoms in this structural molecule.
      */
@@ -250,6 +321,10 @@ class StrcMolecule {
     var size: Int {
         return atoms.count
     }
+    
+    mutating func addAtom(_ atom: Atom){
+        atoms.insert(atom)
+    }
 }
 
 extension StrcMolecule: Hashable {
@@ -263,4 +338,126 @@ extension StrcMolecule: Hashable {
         hasher.combine(atoms)
         hasher.combine(bondGraphs)
     }
+}
+
+
+/**
+ Resign the rvec for a list of atoms
+ */
+func findPossibleAtoms(_ atoms: [Atom]) -> [Atom] {
+    var possibleList: [Atom] = []
+    for atom in atoms {
+        guard atom.rvec != nil else {
+            continue
+        }
+        let possibleRvecList = atom.rvec!.resign()
+        for possibleRvec in possibleRvecList {
+            let newAtom = Atom(atom.name, possibleRvec)
+            possibleList.append(newAtom)
+        }
+    }
+    return possibleList
+}
+
+/**
+ Calculate the distance between two atoms
+ */
+func atomDistance(_ atom1: Atom, _ atom2: Atom) -> Double?{
+    guard atom1.rvec != nil && atom2.rvec != nil else {
+        return nil
+    }
+    let dvec = atom1.rvec! - atom2.rvec!
+    let d = sqrt(dvec .* dvec)
+    return d
+}
+
+/**
+ Filtering by bond length with the reference of bond type
+ */
+func bondTypeLengthFilter(_ atom1: Atom, _ atom2: Atom, _ bondType: ChemBondType, _ tolRange: Double = 0.1) -> Bool {
+    let d = atomDistance(atom1, atom2)
+    guard d != nil && bondType.validate() else {
+        return false
+    }
+    if d! > (bondType.length! - tolRange) && d! < (bondType.length! + tolRange) {
+        return true
+    }
+    else {
+        return false
+    }
+}
+
+/**
+ Find possible bond types between two atom names
+ */
+func possibleBondTypes(_ atomName1: String, _ atomName2: String) -> [ChemBondType] {
+    var possibleBondTypeList: [ChemBondType] = []
+    for order in 1...4 {
+        let bond = ChemBondType(atomName1, atomName2, order)
+        if bond.validate() {
+            possibleBondTypeList.append(bond)
+        }
+    }
+    return possibleBondTypeList
+}
+
+/**
+ Molecule constructor for an atom. One atom is compared with a valid structural molecule. The atom will be added to the structural molecule. If the atom is valid to be connected through certain bonds to the structural molecule, that bond will be added to the existing bond graphs. Otherwise, the bond graphs will be empty.
+ 
+ - Parameter stMol: The existing valid structural molecule.
+ 
+ - Parameter atom: The atom to test with the structural molecule `stMol`.
+ 
+ - Parameter tolRange: The tolerance level, unit in angstrom.
+ 
+ */
+
+func bondLengthStrcMoleculeConstructor(stMol: StrcMolecule, atom: Atom, tolRange: Double = 0.1) -> StrcMolecule {
+    var mol = stMol
+    let bondGraphs = mol.bondGraphs
+    
+    if mol.size <= 0 {
+        mol.addAtom(atom)
+    }
+    else {
+        mol.bondGraphs.removeAll()
+        
+        for vAtom in stMol.atoms {
+            let possibleBts = possibleBondTypes(vAtom.name, atom.name)
+            for bondType in possibleBts {
+                guard bondType.validate() else {
+                    continue
+                }
+                var dPass = false
+                if bondTypeLengthFilter(vAtom, atom, bondType, tolRange) {
+                    let vRemainingAtoms = stMol.atoms.filter({$0 != vAtom})
+                    dPass = true
+                    for vRAtom in vRemainingAtoms {
+                        guard let d: Double = atomDistance(vRAtom, atom) else {
+                            dPass = false
+                            continue
+                        }
+                        if d < (bondType.length! - tolRange) {
+                            dPass = false
+                        }
+                    }
+                    if dPass {
+                        let pBond = ChemBond(vAtom, atom, bondType)
+                        mol.addAtom(atom)
+                        if stMol.size == 1 {
+                            mol.bondGraphs.insert(ChemBondGraph(Set([pBond])))
+                        }
+                        else if stMol.size > 1 {
+                            for bondGraph in bondGraphs {
+                                var pBondGraph = bondGraph
+                                pBondGraph.bonds.insert(pBond)
+                                mol.bondGraphs.insert(pBondGraph)
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    return mol
 }
--- JYMoleculeTool-Swift/main.swift
+++ JYMoleculeTool-Swift/main.swift
@@ -8,11 +8,15 @@
 
 import Foundation
 
+//
+// Currently a test ground to test everything
+//
+
 print("Hello, World!")
 
-//let vec1 = Vector(1,2,3)
-//let vec2 = Vector(2,3,4)
-//let vec3 = Vector(1,2,3)
+//let vec1 = Vector3D(1,2,3)
+//let vec2 = Vector3D(2,3,4)
+//let vec3 = Vector3D(1,2,3)
 //
 //print(vec1 == vec2)
 //print(vec1 == vec3)
@@ -26,6 +30,10 @@ print("Hello, World!")
 //let bond = ChemBondType("C", "C")
 //print(bond.validate())
 
-let bond = ChemBond(Atom("C"), Atom("C"), ChemBondType("C", "C"))
-let bond2 = ChemBond(Atom("C"), Atom("C"), ChemBondType("C", "C", 1))
-print(bond == bond2)
+//let bond = ChemBond(Atom("C"), Atom("C"), ChemBondType("C", "C"))
+//let bond2 = ChemBond(Atom("C"), Atom("C"), ChemBondType("C", "C", 1))
+//print(bond == bond2)
+
+let vec1 = Vector3D(1.5,2.3,3.2)
+let vec2 = Vector3D(1.2,-1.2,9.0)
+print(vec1 .* vec2)
